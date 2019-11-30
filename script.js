aaa5f928e54f46bb55334a8d989bb32900cb96ce
Paras Nagpal <paras.nag@gmail.com>
2019-11-30 16:41:03 +0530

    (feat):tokenize and search

--- script.js
+++ script.js
@@ -1,20 +1,75 @@
 window.onload=function(){
 
-    var documents=document.getElementById("docs")
-    var tokenize=document.getElementById("tokenize")
-    var count=document.getElementById("count")
-
     var reverse_map={}
-    var input=document.getElementById("docs").value
-    
-    var before
+    var map={}
+    var doc_count=0
+    var word_to_doc_map={} 
 
+    
+    //variable to compare initial and final states of input div element 
+    var before
+    
+    //set initial state to current state
     $("#docs").on('focus',function(){
         before=$(this).html()
     })
+    //compare final state with initial state
     $('#docs').on('blur',function(){
+
         if(before!=$(this).html())
-            console.log('changed')    
+            {
+                //doc splitting
+                var count=$(this).html().split('<div><br></div>')
+
+                //map each document to its doc no
+                count.map((str)=>{
+                    reverse_map[str]=doc_count++
+                    map[doc_count-1]=str
+                    
+                    //for each doc split its word and map to doc
+                    var words=str.split(" ")
+                    words.map((word)=>{
+
+                        //crop at ends
+                        while(word.slice(-1)=='.' || word.slice(-1)==',' || word.slice(-1)==')' ||word.slice(-1)=='!')
+                            word=word.substring(0,word.length-1)
+                        
+                        //crop in front
+                        while(word.slice(0)=='.' || word.slice(0)==',' || word.slice(0)=='(' ||word.slice(0)=='!')
+                            word=word.substring(1,word.length-1)
+
+                          //initialize array if not there
+                        if(!word_to_doc_map[word])
+                            word_to_doc_map[word]=[]
+
+                        word_to_doc_map[word].push(doc_count-1)    
+                        
+                        console.log(word_to_doc_map)
+                    })
+
+                })
+            }
+
     })
+
+    //search logic
+    $('#search').click(()=>{
+        
+        var search_word=$('#search_word').val()
+        var count_docs=0
+        var printed_docs={}
+
+        if(word_to_doc_map[search_word])
+
+            word_to_doc_map[search_word].map(x=>{
+                if(!printed_docs[x] && count_docs<10)
+                    {
+                        $('#search_result').append(map[x])
+                        count_docs++
+                    }
+                printed_docs[x]=1
+            })
+    })
+
     
 }
